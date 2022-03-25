1cd59790d8118a1639d8a9a775980a5b4f19544d
ReiPano <reipano33@yahoo.com>
2022-03-25 16:21:40 +0100

    Added measurement unit card

--- src/views/metadata/structural/StructuralComponents.vue
+++ src/views/metadata/structural/StructuralComponents.vue
@@ -164,6 +164,29 @@
         </CCardBody>
       </CCard>
     </CCol>
+    <CCol xs>
+      <CCard>
+        <CCardBody>
+          <CCardTitle>
+            <span>{{ $t("structural.measurementUnit_title") }}</span>
+          </CCardTitle>
+          <CCardText>
+            <span>{{ $t("structural.measurementUnit_text") }}</span>
+          </CCardText>
+          <CButton>
+            <router-link
+              tag="a"
+              :to="{ name: 'MeasurementUnit' }"
+              class="text-decoration-none text-primary"
+              ><span class="icon-span"
+                >{{ $t("structural.measurementUnit_link") }}&nbsp;</span
+              >
+              <CIcon name="cil-arrow-right" />
+            </router-link>
+          </CButton>
+        </CCardBody>
+      </CCard>
+    </CCol>
     <!--
     <div class="col-sm-6 col-md-4">
       <div class="card">
