ea972aaec0ec2fd9e61b8fd66c12c3f0c4ed6411
Florian Nika <florianika@gmail.com>
2022-04-27 16:27:39 +0200

    adding read data structures

---
+++ src/services/structural/dataStructure.service.js
@@ -0,0 +1,113 @@
+import { axiosIais } from "@/http";
+//
+
+export const dataStructureService = {
+  findAll,
+  findById,
+  save,
+  update,
+  delete: _delete,
+};
+
+function findAll() {
+  return new Promise((resolve, reject) => {
+    axiosIais.get("/structural/OpenDataStructures").then(
+      (response) => {
+        resolve(response.data.dataStructures);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+}
+function findById(id) {
+  return new Promise((resolve, reject) => {
+    axiosIais.get("/structural/OpenDataStructures/" + id).then(
+      (response) => {
+        var data = response.data.dataStructure
+          ? response.data.dataStructure
+          : null;
+        //console.log(data);
+        resolve(data);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+}
+function save(formData) {
+  return new Promise((resolve, reject) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const requestBody = {
+      name: formData.name,
+      description: formData.description,
+      definition: formData.definition,
+      localId: formData.localId,
+    };
+
+    axiosIais.post("structural/ClosedDataStructures", requestBody, config).then(
+      (response) => {
+        //console.log(response.data);
+        resolve(response.data);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+}
+
+function update(formData) {
+  return new Promise((resolve, reject) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const requestBody = {
+      name: formData.name,
+      description: formData.description,
+      definition: formData.definition,
+      localId: formData.localId,
+      version: formData.version ? formData.version : "",
+    };
+
+    axiosIais
+      .put(
+        "structural/ClosedDataStructures/" + formData.id,
+        requestBody,
+        config
+      )
+      .then(
+        (response) => {
+          //console.log(response.data);
+          resolve(response.data);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+  });
+}
+
+function _delete(id) {
+  return new Promise((resolve, reject) => {
+    axiosIais.delete("structural/ClosedDataStructures/" + id).then(
+      (response) => {
+        //console.log(response.data);
+        resolve(response.data);
+      },
+      (error) => {
+        reject(error);
+      }
+    );
+  });
+}
---
+++ src/views/metadata/structural/dataStructure/DataStructureView.vue
@@ -0,0 +1,223 @@
+<template>
+  <CRow v-if="dataStructure">
+    <div
+      class="jumbotron jumbotron-fluid col-lg-12 p-2 mb-3"
+      v-if="dataStructure"
+    >
+      <div class="p-3">
+        <h2 class="display-5">
+          <span>{{ dataStructure.name }}</span>
+          <span class="lead">
+            ({{ dataStructure.localId }} {{ dataStructure.version }} )</span
+          >
+        </h2>
+
+        <p class="lead" v-if="dataStructure.description">
+          <span>
+            <strong>{{ $t("structural.description") }}:</strong>
+            {{ dataStructure.description }}
+          </span>
+        </p>
+        <p class="lead" v-if="dataStructure.group">
+          <span>
+            <strong>{{ $t("structural.dataset.group") }}:</strong>
+            {{ dataStructure.group }}
+          </span>
+        </p>
+      </div>
+    </div>
+    <CRow>
+      <CCol class="col-12">
+        <CCard class="mb-3">
+          <CCardHeader class="bg-white">
+            <CCardTitle> Records </CCardTitle>
+          </CCardHeader>
+          <CCardBody>
+            <CCardText>
+              <CRow>
+                <CCol
+                  :sm="6"
+                  v-for="record in dataStructure.records"
+                  :key="record.id"
+                >
+                  <CCard class="mb-3 mt-3" color="light">
+                    <CCardBody>
+                      <CCardTitle>
+                        <span> {{ record.name }}</span>
+                      </CCardTitle>
+                      <CCardText>
+                        <CRow class="mb-3">
+                          <CCol class="col-sm-12">
+                            <CFormLabel for="tableName" class="col-form-label">
+                              {{ $t("structural.dataset.record.table_name") }}
+                            </CFormLabel>
+                            <CFormInput
+                              type="text"
+                              id="tableName"
+                              :value="record.localId"
+                              readonly
+                            />
+                          </CCol>
+                        </CRow>
+                        <CRow v-if="record.parent" class="mb-3">
+                          <CCol class="col-sm-12">
+                            <CFormLabel
+                              for="parentRecord"
+                              class="col-form-label"
+                            >
+                              {{ $t("structural.dataset.record.parent") }}
+                            </CFormLabel>
+                            <CFormInput
+                              type="text"
+                              id="parentRecord"
+                              :value="record.parent.name"
+                              readonly
+                            />
+                          </CCol>
+                        </CRow>
+                        <CRow>
+                          <CCol class="col-sm-12">
+                            <CFormLabel for="unitType" class="col-form-label">
+                              {{ $t("structural.dataset.record.unit_type") }}
+                            </CFormLabel>
+                            <CFormInput
+                              type="text"
+                              id="unitType"
+                              :value="record.unitType.name"
+                              readonly
+                            />
+                          </CCol>
+                        </CRow>
+                      </CCardText>
+                    </CCardBody>
+                  </CCard>
+                </CCol>
+              </CRow>
+            </CCardText>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+    <CRow>
+      <CCol>
+        <CCard>
+          <CCardHeader class="bg-white">
+            <CCardTitle> Components </CCardTitle>
+          </CCardHeader>
+          <CCardBody>
+            <CCardText>
+              <div class="table-responsive">
+                <CSmartTable
+                  v-if="dataStructure.components"
+                  :items="dataStructure.components"
+                  :activePage="1"
+                  header
+                  :columns="columns"
+                  columnFilter
+                  cleaner
+                  itemsPerPageSelect
+                  :itemsPerPage="5"
+                  columnSorter
+                  :sorterValue="{
+                    column: 'localId',
+                    state: 'asc',
+                  }"
+                  pagination
+                >
+                  <template #show_representation="{ item }">
+                    <td class="py-2">
+                      <CButton
+                        color="primary"
+                        variant="outline"
+                        square
+                        size="sm"
+                        @click="showRepresentation(item)"
+                      >
+                        Show
+                      </CButton>
+                    </td>
+                  </template>
+                </CSmartTable>
+              </div>
+            </CCardText>
+            <CButton
+              color="primary"
+              size="sm"
+              @click="handleBack()"
+              :disabled="disabled"
+              >Back</CButton
+            >
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+  </CRow>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "DataStructureView",
+  data() {
+    return {
+      columns: [
+        {
+          key: "localId",
+          label: "Column",
+        },
+        {
+          key: "name",
+          label: "Name",
+        },
+        {
+          key: "type",
+          label: "Type",
+        },
+        {
+          key: "show_representation",
+          label: "Representation",
+          _style: { width: "1%" },
+          filter: false,
+          sorter: false,
+        },
+      ],
+      disabled: false,
+    };
+  },
+  computed: {
+    ...mapGetters("dataStructure", ["dataStructure"]),
+  },
+  methods: {
+    handleBack() {
+      this.disabled = true; //disable button
+      this.$router.push("/metadata/structural/dataStructures");
+    },
+    showRepresentation(item) {
+      this.$router.push(item.representationLink);
+    },
+  },
+  created() {
+    this.$store.dispatch("dataStructure/findById", this.$route.params.id);
+  },
+};
+</script>
+<style scoped>
+.form-control:disabled,
+.form-control[readonly] {
+  background-color: #ebedef;
+  opacity: 1;
+}
+</style>
+<style scoped>
+.jumbotron {
+  border: 1px solid #ced2d8;
+  box-shadow: 0 1px 1px 0 rgba(60, 75, 100, 0.14),
+    0 2px 1px -1px rgba(60, 75, 100, 0.12), 0 1px 3px 0 rgba(60, 75, 100, 0.2);
+}
+@media (min-width: 576px) {
+  .jumbotron {
+    padding: 1.5rem 2rem;
+  }
+}
+</style>
